b6137a8b5b2ea3e39c70125b1daae46cca2156de
NaeLike เนไลก์ <87599521+Naelike@users.noreply.github.com>
2021-07-18 22:35:32 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>API Pay Truemoney Gift V100 Naelike | PHP</title>
+  </head>
+  <body>
+    <h1>API Pay Truemoney Gift V100 Naelike</h1>
+    <br>
+    <h4>PHP</h4>
+    <?php
+      $get_phone = $_GET['phone'];
+      $gte_paygift = $_GET['paygift'];
+      $url_get = '../pay-truemoney/gift' . '?phone=' . $get_phone . '&paygift=' . $gte_paygift;
+      $api_get_json = file_get_contents($url_get);
+      $json_jq = json_decode($api_get_json);
+      echo json_encode($json_jq);
+    ?>
+  </body>
+</html>
